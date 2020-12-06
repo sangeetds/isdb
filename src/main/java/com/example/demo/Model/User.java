8dd4d094c370c004ce1c9315d42ae711fae7fdcc
sangeetds <sangeetnarayands@gmail.com>
2020-12-07 01:06:57 +0530

    -Initial commit

---
+++ src/main/java/com/example/demo/Model/User.java
@@ -0,0 +1,89 @@
+package com.example.demo.Model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    private @Id @GeneratedValue(strategy = GenerationType.AUTO) long id;
+    private @NotBlank String username;
+    private @NotBlank String password;
+    private @NotBlank @Email String email;
+    private boolean loggedIn;
+
+    public User() {
+  ***REMOVED***
+
+    public User(@NotBlank String username,
+                @NotBlank String password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = false;
+  ***REMOVED***
+
+    public long getId() {
+        return this.id;
+  ***REMOVED***
+
+    public String getUsername() {
+        return this.username;
+  ***REMOVED***
+
+    public void setUsername(String username) {
+        this.username = username;
+  ***REMOVED***
+
+    public String getPassword() {
+        return this.password;
+  ***REMOVED***
+
+    public void setPassword(String password) {
+        this.password = password;
+  ***REMOVED***
+
+    public String getEmail() {
+        return email;
+  ***REMOVED***
+
+    public void setEmail(String email) {
+        this.email = email;
+  ***REMOVED***
+
+    public boolean isLoggedIn() {
+        return this.loggedIn;
+  ***REMOVED***
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+  ***REMOVED***
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return username.equals(user.username) &&
+                password.equals(user.password) &&
+                email.equals(user.email);
+  ***REMOVED***
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, email, loggedIn);
+  ***REMOVED***
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", loggedIn=" + loggedIn +
+                '***REMOVED***';
+  ***REMOVED***
+***REMOVED***
---
+++ src/main/java/com/example/demo/Status.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public enum Status {
+    SUCCESS,
+    USER_ALREADY_EXISTS,
+    FAILURE
+***REMOVED***
