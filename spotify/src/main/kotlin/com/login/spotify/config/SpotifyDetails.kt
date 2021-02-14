7a83f543ac201552ed435faf46abc8f5d187343a
sangeetds <sangeetnarayands@gmail.com>
2021-02-14 19:21:24 +0530

    heroku commit.

---
+++ spotify/src/main/kotlin/com/login/spotify/config/SpotifyDetails.kt
@@ -0,0 +1,10 @@
+package com.login.spotify.config
+
+***REMOVED***
+  ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+        const val spotifyTrackApiUrl = "https://api.spotify.com/"
+  ***REMOVED***
+***REMOVED***
