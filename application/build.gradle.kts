7a7bff5ccc57b8161b1f216e563cecc805b2df12
sangeetds <sangeetnarayands@gmail.com>
2021-02-15 21:12:39 +0530

    Gradle updated.

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -32,6 +32,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.4.0")
     implementation("org.springframework.boot:spring-boot-starter-validation:2.4.0")
 
+    implementation("javax.xml.bind:jaxb-api:2.3.0")
+    implementation("com.sun.xml.bind:jaxb-core:2.3.0")
+    implementation("com.sun.xml.bind:jaxb-impl:2.3.0")
+    implementation("javax.activation:activation:1.1.1")
+    implementation ("jakarta.xml.bind:jakarta.xml.bind-api:2.3.2")
+    implementation ("org.glassfish.jaxb:jaxb-runtime:2.3.2")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("org.springframework.security:spring-security-web:5.4.5")
 ***REMOVED***
