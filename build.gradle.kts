4918001138982ed8e5e9a392f0f63463fc90dba7
sangeetds <sangeetnarayands@gmail.com>
2021-03-02 21:55:36 +0530

    Migrated to PostgreSQL.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
     kotlin("jvm") version "1.4.30"
     kotlin("plugin.spring") version "1.4.30"
     kotlin("plugin.jpa") version "1.4.30"
@@ -21,6 +22,10 @@ dependencies {
     val moshiVersion = "1.11.0"
     val retrofitVersion = "2.9.0"
 
+    // kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     //retrofit and moshi
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
@@ -34,18 +39,26 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
 
+    // spring web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+
+    // spring data
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web-services")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // postgres
+    runtimeOnly("org.postgresql:postgresql:9.4-1206-jdbc42")
+
     runtimeOnly("com.h2database:h2")
-    runtimeOnly("mysql:mysql-connector-java")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 ***REMOVED***
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/isdb/model/User.kt
+++ src/main/kotlin/com/isdb/model/User.kt
@@ -12,20 +12,9 @@ import javax.validation.constraints.NotNull
 @Entity
 @Table(name = "user")
 data class User (
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO) var id: Long = 0,
+    @Id @Column(name = "id") @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Int = 0,
     @NotNull @Column(name = "username") var username: String,
     @NotNull @Column(name = "password") var password:  String,
     @NotNull @Email @Column(name = "email") var email: String,
     @Column(name = "logged_in") var isLoggedIn: Boolean
-) {
-
-    constructor() : this(
-        0,
-        ('a'..'z').random().toString(),
-        ('a'..'z').random().toString(),
-        "${('a'..'z').random()***REMOVED***@${('a'..'z').random()***REMOVED***",
-        false
-    )
-***REMOVED***
+)
