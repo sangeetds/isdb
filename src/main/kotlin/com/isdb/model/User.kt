988b5111ab329d9dd579a15f9a042b213a45dd3d
sangeetds <sangeetnarayands@gmail.com>
2021-03-02 10:48:17 +0530

    new rebase.

--- src/main/kotlin/com/isdb/model/User.kt
+++ src/main/kotlin/com/isdb/model/User.kt
@@ -12,9 +12,9 @@ import javax.validation.constraints.NotNull
 @Entity
 @Table(name = "user")
 data class User (
-    @Id @Column(name = "id") @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long = 0,
-    @NotNull @Column(name = "username") var username: String,
-    @NotNull @Column(name = "password") var password:  String,
-    @NotNull @Email @Column(name = "email") var email: String,
-    @Column(name = "logged_in") var isLoggedIn: Boolean
+    @Id @Column(name = "id") @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Int = 0,
+    @NotNull @Column(name = "username") var username: String = "",
+    @NotNull @Column(name = "password") var password:  String = "",
+    @NotNull @Email @Column(name = "email") var email: String = "",
+    @Column(name = "logged_in") var isLoggedIn: Boolean = false
 )
